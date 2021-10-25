ec7b9322a9e4828bab725089fc68ee1296eee716
Mini-Cynthia <89430952+Mini-Cynthia@users.noreply.github.com>
2021-10-25 14:46:24 +0800

    initialize by upload files

---
+++ src/main/java/com/zuyi/zuyiHR/ZuyiHrApplication.java
@@ -0,0 +1,13 @@
+package com.zuyi.zuyiHR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuyiHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuyiHrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuyi/zuyiHR/ZuyiHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuyi.zuyiHR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuyiHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
